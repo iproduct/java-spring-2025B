ec23dbe97939205291946fef022f56f318f238bd
Trayan Iliev <office@iproduct.org>
2025-06-16 17:23:43 +0300

    Person class completed

--- 01-java-intro/src/Main.java
+++ 01-java-intro/src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import course.spring.model.Person;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello from Java!\n");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("John", "Smith", LocalDate.of(1978, 7, 18)));
+        persons.add(new Person("Ivan", "Petrov", LocalDate.of(1982, 8, 3)));
+        persons.add(new Person("Maria", "Hristova", LocalDate.of(1995, 2, 11)));
+        persons.forEach(System.out::println);
     }
 }
