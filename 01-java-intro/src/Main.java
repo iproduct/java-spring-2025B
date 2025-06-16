1b72b53f5db2f39d887d1991a37e48db60a0d625
Trayan Iliev <office@iproduct.org>
2025-06-16 18:18:50 +0300

    main modified to use User

--- 01-java-intro/src/Main.java
+++ 01-java-intro/src/Main.java
@@ -1,4 +1,5 @@
 import course.spring.model.Person;
+import course.spring.model.User;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -6,18 +7,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Person> persons = new ArrayList<>();
-        persons.add(new Person("John", "Smith", LocalDate.of(1978, 7, 18)));
-        persons.add(new Person("Ivan", "Petrov", LocalDate.of(1982, 8, 3)));
-        persons.add(new Person("Maria", "Hristova", LocalDate.of(1995, 2, 11)));
-//        persons.forEach(System.out::println);
+        List<User> user = new ArrayList<>();
+        user.add(new User("John", "Smith", "john", "john123"));
+        user.add(new User("Ivan", "Petrov", "ivan", "ivan123"));
+        user.add(new User("Maria", "Hristova", "maria", "maria123"));
+//        user.forEach(System.out::println);
 
-        for(Person p : persons) {
-            System.out.println(p);
+        for(User u : user) {
+            System.out.println(u);
         }
         System.out.println("====================================");
-        for(int i = 0; i < persons.size(); i++){
-            System.out.println(persons.get(i));
+        for(int i = 0; i < user.size(); i++){
+            System.out.println(user.get(i));
         }
     }
 }
