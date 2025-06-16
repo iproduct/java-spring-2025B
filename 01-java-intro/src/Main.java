d7ac465aa12e6c2d3a4f6f5e0efc5be19398897a
Trayan Iliev <office@iproduct.org>
2025-06-16 18:14:29 +0300

    User class added

--- 01-java-intro/src/Main.java
+++ 01-java-intro/src/Main.java
@@ -10,6 +10,14 @@ public class Main {
         persons.add(new Person("John", "Smith", LocalDate.of(1978, 7, 18)));
         persons.add(new Person("Ivan", "Petrov", LocalDate.of(1982, 8, 3)));
         persons.add(new Person("Maria", "Hristova", LocalDate.of(1995, 2, 11)));
-        persons.forEach(System.out::println);
+//        persons.forEach(System.out::println);
+
+        for(Person p : persons) {
+            System.out.println(p);
+        }
+        System.out.println("====================================");
+        for(int i = 0; i < persons.size(); i++){
+            System.out.println(persons.get(i));
+        }
     }
 }
---
+++ 01-java-intro/src/course/spring/model/User.java
@@ -0,0 +1,52 @@
+package course.spring.model;
+
+import static course.spring.model.Role.*;
+
+public class User extends Person {
+    private String username;
+    private String password;
+    private Role role = READER;
+    private String email;
+
+    public User() {
+        super();
+    }
+
+    public User(String firstName, String lastName, String username, String password) {
+        super(firstName, lastName);
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
