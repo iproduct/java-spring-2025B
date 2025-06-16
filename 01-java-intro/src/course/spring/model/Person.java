fe4b801d71ff96ab53fdfe9006152f4751544682
Trayan Iliev <office@iproduct.org>
2025-06-16 15:32:11 +0300

    Person model added

---
+++ 01-java-intro/src/course/spring/model/Person.java
@@ -0,0 +1,10 @@
+package course.spring.model;
+
+import java.time.LocalDate;
+
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+}
