52a9357cc23df3907334d2564234aba3191a37bf
Trayan Iliev <office@iproduct.org>
2025-06-23 17:15:45 +0300

    UserServiceImpl added

---
+++ 03-spring-gradle/src/main/java/course/spring/domain/impl/UserServiceImpl.java
@@ -0,0 +1,63 @@
+package course.spring.domain.impl;
+
+import course.spring.dao.UserRepository;
+import course.spring.domain.UserService;
+import course.spring.exception.NonexistingEntityException;
+import course.spring.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepo;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepo.findById(id).orElseThrow(() -> new NonexistingEntityException(
+                String.format("User with ID='%s' does not exist.", id)
+        ));
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepo.findByUsername(username).orElseThrow(() -> new NonexistingEntityException(
+                String.format("User with username '%s' does not exist.", username)
+        ));
+    }
+
+    @Override
+    public User addUser(User user) {
+        return userRepo.create(user);
+    }
+
+    @Override
+    public User updateUser(User user) {
+        return userRepo.update(user).orElseThrow(() -> new NonexistingEntityException(
+                String.format("Can not update non-existing user '%s' with ID='%d'", user.getUsername(), user.getId())
+        ));
+    }
+
+    @Override
+    public User deleteUserById(Long id) {
+        return userRepo.deleteById(id).orElseThrow(() -> new NonexistingEntityException(
+                String.format("Can not delete non-existing user with ID='%d'", id)
+        ));
+    }
+
+    @Override
+    public long getUsersCount() {
+        return userRepo.count();
+    }
+}
