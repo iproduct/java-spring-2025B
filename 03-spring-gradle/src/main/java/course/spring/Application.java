62484520cf3eb301c15857c15ac47edb60991900
Trayan Iliev <office@iproduct.org>
2025-06-24 15:29:57 +0300

    app-context.xml added

--- 03-spring-gradle/src/main/java/course/spring/Application.java
+++ 03-spring-gradle/src/main/java/course/spring/Application.java
@@ -2,8 +2,10 @@ package course.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("app-config.xml")
 public class Application {
 
 	public static void main(String[] args) {
